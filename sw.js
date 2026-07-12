734fe6535461a45f9f86b7cd6a8cff47b7409161
rileysiu <rileysiu23@gmail.com>
2026-07-12 21:17:45 +0800

    Narrow card title wrap width and bump cache version
    Reduce .card-title-text max-width so long titles wrap sooner.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kancollect-2026-06-08-e';
+const CACHE = 'kancollect-2026-07-12-a';
 const ASSETS = ['./','./index.html','./app.js','./styles.css','./wallpaper.jpg'];
 
 self.addEventListener('install', e => {
