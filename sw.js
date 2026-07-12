2d8a24c0b8f04ff3dd8b16f7555c06dae574beba
rileysiu <rileysiu23@gmail.com>
2026-07-12 21:26:51 +0800

    Fix card title wrap not working in Safari and bump cache version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kancollect-2026-07-12-b';
+const CACHE = 'kancollect-2026-07-12-c';
 const ASSETS = ['./','./index.html','./app.js','./styles.css','./wallpaper.jpg'];
 
 self.addEventListener('install', e => {
